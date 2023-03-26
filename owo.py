08b297fc3f72478d340967e8735de1b39ac1d9cb
HenHen102 <128989724+HenHen102@users.noreply.github.com>
2023-03-26 16:50:23 -0400

    Create owo.py

---
+++ owo.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "MTA4OTU3MjAzMjM0MzE4NzU1Nw.GZdi0S.bO8WYu8s05sllUT9w4vSz0wLBqcMxiHXnuDIiY"
+
+class OwO(commands.Bot):
+    def __init__(self, command_prefix):
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        commands.Bot.__init__(self, command_prefix=command_prefix, intents=intents)
+
+        self.register_commands()
+
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+        
+    def owo(str):
+      while(str.find("l") != -1):
+          str = str[:str.index("l")] + "w" + str[str.index("l")+1:]
+      while(str.find("r") != -1):
+          str = str[:str.index("r")] + "w" + str[str.index("r")+1:]
+      return str
+    
+    def register_commands(self):
+        @self.event
+        async def on_message(messsage):
+            await message.channel.send(owo(message.content))
+            
+
+client = OwO("!owo ")
+client.run(TOKEN)
